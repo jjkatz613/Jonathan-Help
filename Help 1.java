329e50bdffe10c3e08287760087bd448b75d1869
katzaz10 <31491542+katzaz10@users.noreply.github.com>
2020-11-25 17:16:10 -0500

    Update Help 1.java
    brackets around if

--- Help 1.java
+++ Help 1.java
@@ -10,7 +10,7 @@ public class U3_L7_Activity_Two{
     
     String word1 = scan.nextLine(), word2 = scan.nextLine();
     
-    if (word1.equals(word2)) System.out.println("Great!");
+    if (word1.equals(word2)) { System.out.println("Great!"); }
     else if (word1.length() == word2.length() && word1.substring(0 , word1.length -1).equal(word2.substring(0, word2.length - 1))) System.out.println("Close Enough");
     else System.out.println("Try again");
   }
